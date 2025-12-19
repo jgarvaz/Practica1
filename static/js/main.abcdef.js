4dc8e77eba6c596782d599ff4b7f32e4ddff5fb2
jgarvaz <jgarvaz2505@g.educaand.es>
2025-12-19 22:20:30 +0100

    aplicacion react y jenkinsfilev2

--- static/js/main.abcdef.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const root = document.getElementById("root");
-
-  root.innerHTML = `
-    <h1>Hola desde React 👋</h1>
-    <p>Aplicación React ejecutada desde Jenkins</p>
-  `;
-})();
-	
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+(function () {
+  const root = document.getElementById("root");
+
+  root.innerHTML = `
+    <h1>Hola React</h1>
+    <p>Aplicación React compilada (build)</p>
+    <p>Lista para Jenkins</p>
+  `;
+})();
