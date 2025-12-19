17fcb71125c91fed85246ce867efeeb1cc93ea80
jgarvaz <jgarvaz2505@g.educaand.es>
2025-12-19 22:30:44 +0100

    app bien hecha

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const root = document.getElementById("root");
-
-  root.innerHTML = `
-    <h1>Hola React</h1>
-    <p>Aplicación React compilada (build)</p>
-    <p>Lista para Jenkins</p>
-  `;
-})();
