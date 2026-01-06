c8bc55cdab695f2c0cf94d02169d93c8d0207217
jgarvaz <jgarvaz2505@g.educaand.es>
2026-01-06 20:58:49 +0100

    app bien hecha

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 React + Jenkins</h1>
+      <p>Pipeline de construcción automática funcionando</p>
+    </div>
+  );
+}
+
+
+
+
+export default App;
